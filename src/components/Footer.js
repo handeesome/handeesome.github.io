22a3c231f59531a400c815a3b2851292cd486388
Dee Du <ducenhandee@gmail.com>
2024-02-10 00:33:10 +0100

    footer adapts dark mode and change position to center

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,15 +2,15 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
 
-const Footer = () => {
+const Footer = ({ isDarkMode }) => {
   return (
-    <footer className="container-fluid bg-dark text-light">
-      <div className="row">
-        <div className="col-md-6">
+    <footer className={`container ${isDarkMode ? "text-light" : "text-dark"}`}>
+      <div className="row justify-content-md-center">
+        <div className="col-md-5 ">
           <p>&copy; 2024 Cenhan Du. All Rights Reserved.</p>
           {/* Add more footer content, links, or social media icons as needed */}
         </div>
-        <div className="col-md-6 justify-content-end d-flex list-unstyled">
+        <div className="col-md-5 justify-content-end d-flex list-unstyled">
           <li className="ms-3">
             <a href="https://www.linkedin.com/in/cenhan-du-5251111ba/">
               <FontAwesomeIcon icon={faLinkedin} className="fa-2x" />
