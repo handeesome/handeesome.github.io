f30ff6e9cadee533007ed7eeca2ba12faa853ac7
Dee Du <ducenhandee@gmail.com>
2025-08-16 05:47:40 +0800

    fix path typo

--- src/pages/BookShelf/MyBookShelf.jsx
+++ src/pages/BookShelf/MyBookShelf.jsx
@@ -1,7 +1,7 @@
 // src/pages/BookShelf/MyBooks.jsx
 import books from "../../data/books/books.json";
 import BookShelf from "../../components/bookShelf/BookShelf";
-import HideBtnsContext from "../../components/bookShelf/hideBtnsContext";
+import HideBtnsContext from "../../components/bookShelf/HideBtnsContext";
 
 const MyBookShelf = () => {
   return (
