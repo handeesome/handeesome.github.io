2cda2cd2e0850e67fc08a82306bd4af5393ab656
handeesome <handeesome@users.noreply.github.com>
2026-03-28 09:28:41 +0000

    deploy: 4ed99ea17bb72a49116dd8b10f2e62ef9d21344d

--- assets/index-BHl_nW1Z.js
+++ assets/index-BHl_nW1Z.js
@@ -1,4 +1,4 @@
-import{g as wt,u as zi,j as le,B as Jr,a as Qr,b as Bi,r as Nt,_ as Mt}from"./index-BT67m2HM.js";import{r as ji}from"./index-84k9TH_T.js";const Gr=`---
+import{g as wt,u as zi,j as le,B as Jr,a as Qr,b as Bi,r as Nt,_ as Mt}from"./index-D0qXOZ9c.js";import{r as ji}from"./index-84k9TH_T.js";const Gr=`---
 title: CCG Auto PPTX Generator
 date: 2025-03-22
 tags: [python, flask, python-pptx]
