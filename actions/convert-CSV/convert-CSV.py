020271dd0284ed866a1e0a38356ea850c923cf80
Dee Du <ducenhandee@gmail.com>
2026-04-18 22:27:23 +0800

    fix books path

--- actions/convert-CSV/convert-CSV.py
+++ actions/convert-CSV/convert-CSV.py
@@ -3,9 +3,9 @@ import json
 from pathlib import Path
 
 # Input and output files
-input_csv = Path("src/data/books/books.csv").resolve()
-output_json = Path("src/data/books/books.json").resolve()
-modification_file = Path("src/data/books/books_modifications.json").resolve()  # <- your overrides
+input_csv = Path("src/static/books/books.csv").resolve()
+output_json = Path("src/static/books/books.json").resolve()
+modification_file = Path("src/static/books/books_modifications.json").resolve()  # <- your overrides
 # Fields mapping: target_key -> source_header
 field_map = {
     "id": "Book Id",
