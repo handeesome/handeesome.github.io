022afc0c65bd318d70e7f6a0a852398158862208
Dee Du <ducenhandee@gmail.com>
2026-04-18 17:31:06 +0800

    move folder

--- src/routes/gameRoutes.jsx
+++ src/routes/gameRoutes.jsx
@@ -2,7 +2,7 @@ import { lazy } from "react";
 import { Route } from "react-router-dom";
 import Board from "../features/profile/components/Board.jsx";
 
-const Sudoku = lazy(() => import("../pages/Games/Sudoku/Sudoku.jsx"));
+const Sudoku = lazy(() => import("../features/games/sudoku/Sudoku.jsx"));
 const Games = lazy(() => import("../pages/Games/Games.jsx"));
 
 const withBoard = (Component, title) => (
