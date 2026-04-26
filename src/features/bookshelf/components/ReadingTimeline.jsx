9290e089946d9986f7e4d139e10260211144a67e
Dee Du <ducenhandee@gmail.com>
2026-04-26 23:22:32 +0800

    WIP add reading timeline route

---
+++ src/features/bookshelf/components/ReadingTimeline.jsx
@@ -0,0 +1,5 @@
+import Board from "../../profile/components/Board";
+const ReadingTimeline = () => {
+  return <Board title="Reading Timeline" />;
+};
+export default ReadingTimeline;
---
+++ src/pages/bookshelf/ReadingTimeline.jsx
@@ -0,0 +1,5 @@
+import ReadingTimelineComponent from "../../features/bookshelf/components/ReadingTimeline";
+const ReadingTimeline = () => {
+  return <ReadingTimelineComponent />;
+};
+export default ReadingTimeline;
