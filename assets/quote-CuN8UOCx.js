efc2552c80879594f7db381fad232839ec36dff4
handeesome <handeesome@users.noreply.github.com>
2026-05-14 17:02:52 +0000

    deploy: 2b31f7ef2d5083db26327e84f7597268b9e794e9

--- assets/quote-CuN8UOCx.js
+++ assets/quote-CuN8UOCx.js
@@ -1,4 +1,4 @@
-import{G as a}from"./index-C_S2sc8_.js";/**
+import{G as a}from"./index-xZF-j2YF.js";/**
  * @license lucide-react v0.541.0 - ISC
  *
  * This source code is licensed under the ISC license.
