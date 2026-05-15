b401788cbec70d73c1786fb4936fa6d61fcbd2bc
handeesome <handeesome@users.noreply.github.com>
2026-05-15 12:46:13 +0000

    deploy: 104fbe1585e046aaec4de1897534df4152e821f8

--- assets/quote-BEoTaZlJ.js
+++ assets/quote-BEoTaZlJ.js
@@ -1,4 +1,4 @@
-import{G as a}from"./index-4uPLMikT.js";/**
+import{G as a}from"./index-__Fyw80i.js";/**
  * @license lucide-react v0.541.0 - ISC
  *
  * This source code is licensed under the ISC license.
