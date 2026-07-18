516bdce57578f62022a58e0e68c666ee1586e1ff
handeesome <handeesome@users.noreply.github.com>
2026-07-18 10:10:24 +0000

    deploy: 953346b5543e566666bc96d7e75acf49f5e8d7e3

--- assets/EditLibrary-Dd6YfbWA.js
+++ assets/EditLibrary-Dd6YfbWA.js
@@ -1,4 +1,4 @@
-import{G as M,c as y,j as t,u as ee,d as qt,H as u,I as Ze,J as Ge,K as Vt,L as De,Q as Zt,g as Gt,M as Kt,a as Yt,N as Xt,B as Qt}from"./index-fQjJSe5p.js";import{u as Jt,a as en,H as be,B as Ke,M as tn,L as nn}from"./useMediaRoom-CKuxJ0eN.js";import{M as J,u as on,g as rn}from"./useUsers-LJHbCavY.js";import{g as an}from"./bookCovers-BZOOPHY3.js";import{Q as sn}from"./quote-BIKjunBU.js";import"./introductions-Dr4TtLH6.js";import"./GoBackButton-CzngT1ac.js";/**
+import{G as M,c as y,j as t,u as ee,d as qt,H as u,I as Ze,J as Ge,K as Vt,L as De,Q as Zt,g as Gt,M as Kt,a as Yt,N as Xt,B as Qt}from"./index-6c2VKOPM.js";import{u as Jt,a as en,H as be,B as Ke,M as tn,L as nn}from"./useMediaRoom-DJ3oIJgd.js";import{M as J,u as on,g as rn}from"./useUsers-Cbwj5eqs.js";import{g as an}from"./bookCovers-BZOOPHY3.js";import{Q as sn}from"./quote-BrN8hPUU.js";import"./introductions-Dr4TtLH6.js";import"./GoBackButton-CiMyYu5t.js";/**
  * @license lucide-react v0.541.0 - ISC
  *
  * This source code is licensed under the ISC license.
