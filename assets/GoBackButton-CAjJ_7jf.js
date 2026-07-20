5714e6560ff78ecd683c5310ba325caf8b6eec85
handeesome <handeesome@users.noreply.github.com>
2026-07-20 14:59:42 +0000

    deploy: 9379d89f980d302e095a9443b56fad78567296ad

--- assets/GoBackButton-CAjJ_7jf.js
+++
@@ -1,1 +0,0 @@
-import{a as t,j as a}from"./index-BS1VlOKP.js";const r=({defaultDest:n,text:o,preferDefaultDest:s=!1})=>{const i=t();return a.jsxs("button",{className:"btn btn-outline-info",onClick:()=>{s&&n?i(n):window.history.length>1?i(-1):n&&i(n)},children:["← ",o]})};export{r as G};
