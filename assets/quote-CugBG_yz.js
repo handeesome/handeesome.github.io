d82e99a7e24d71fc6856582c431223186e1f97b4
handeesome <handeesome@users.noreply.github.com>
2026-07-20 13:46:33 +0000

    deploy: 34522b7b6ab83f14470d7bfe0440c1a715fd6539

--- assets/quote-CugBG_yz.js
+++ assets/quote-CugBG_yz.js
@@ -1,4 +1,4 @@
-import{G as a}from"./index-D58iWfeu.js";/**
+import{G as a}from"./index-jPJGP0M6.js";/**
  * @license lucide-react v0.541.0 - ISC
  *
  * This source code is licensed under the ISC license.
