25f612160ad520308a505d5daa2b1c6104841188
handeesome <handeesome@users.noreply.github.com>
2026-07-23 11:00:56 +0000

    deploy: 4952825b5a93b01bcfb05f17b287df48ce440a86

--- assets/quote-BMhBDQmv.js
+++ assets/quote-BMhBDQmv.js
@@ -1,4 +1,4 @@
-import{G as a}from"./index-GGu5xZNR.js";/**
+import{G as a}from"./index-CaKWyjJ9.js";/**
  * @license lucide-react v0.541.0 - ISC
  *
  * This source code is licensed under the ISC license.
