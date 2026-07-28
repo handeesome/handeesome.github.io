07e54c9595b40116bfbae2dc2f52d584ee29db85
handeesome <handeesome@users.noreply.github.com>
2026-07-28 11:09:32 +0000

    deploy: 21fb151c8af89c1e450a2bdaf0b705db0aab3e14

---
+++ assets/ScrollToRef-b2XtfMe6.js
@@ -0,0 +1,1 @@
+import{O as r,c as e}from"./index-CaY7-6Kn.js";const a=({scrollRef:o})=>{const{pathname:t}=r();return e.useEffect(()=>{o?.current?.scrollIntoView({behavior:"smooth",block:"start"})},[t,o]),null};export{a as S};
