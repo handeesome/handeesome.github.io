681569d0f55070e5584baa98618e84d9d010b5ec
handeesome <handeesome@users.noreply.github.com>
2026-08-31 16:32:22 +0000

    deploy: 3ae08c348af81f66d547115a9e81d7058d88d9fd

---
+++ assets/ScrollToRef-ChhAJvei.js
@@ -0,0 +1,1 @@
+import{O as r,c as e}from"./index-i0U6rV_O.js";const a=({scrollRef:o})=>{const{pathname:t}=r();return e.useEffect(()=>{o?.current?.scrollIntoView({behavior:"smooth",block:"start"})},[t,o]),null};export{a as S};
