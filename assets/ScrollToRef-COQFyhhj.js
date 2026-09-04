172661be12c8e23a7ae3866f0cf14f318b66f687
handeesome <handeesome@users.noreply.github.com>
2026-09-04 13:01:31 +0000

    deploy: 35f1b64a22bd3296c20073ad99d007eca8be3575

---
+++ assets/ScrollToRef-COQFyhhj.js
@@ -0,0 +1,1 @@
+import{P as r,c as e}from"./index-CUOgcXNA.js";const a=({scrollRef:o})=>{const{pathname:t}=r();return e.useEffect(()=>{o?.current?.scrollIntoView({behavior:"smooth",block:"start"})},[t,o]),null};export{a as S};
