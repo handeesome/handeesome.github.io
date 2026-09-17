d26ae0cac9d34c46502182c8fe9660bf95d44241
handeesome <handeesome@users.noreply.github.com>
2026-09-17 13:57:50 +0000

    deploy: 65d1aa8d874d862d6612ca7afe5700ce356bdd7c

--- assets/quote-DDYEVuxg.js
+++ assets/quote-DDYEVuxg.js
@@ -1,4 +1,4 @@
-import{G as a}from"./index-B3kbHFud.js";/**
+import{G as a}from"./index-C40ksrIe.js";/**
  * @license lucide-react v0.541.0 - ISC
  *
  * This source code is licensed under the ISC license.
